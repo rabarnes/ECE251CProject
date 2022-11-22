7ffa70df95bf4ba130f570c27b9bce6833722797
AidanDougherty <aidan3141@gmail.com>
2022-11-21 21:07:22 -0800

    1 loop, but wavelets not PR?

--- thresholding.m
+++ thresholding.m
@@ -11,11 +11,10 @@ ft_weight = 1/(norm(imdata,"fro"));
 F_imdata = fftshift(fft2(imdata)*ft_weight);
 
 
-%downsample, remove every mth row
-m= 5;
-F_imdata_sp = F_imdata;
-F_imdata_sp(1:m:round(2*end/5),:) = zeros(length(1:m:round(2*rows/5)),cols);
-F_imdata_sp(round(3*end/5):m:end,:) = zeros(length(round(3*rows/5):m:rows),cols);
+%downsample, remove every mth row, keep middle third
+m= 4;
+mask = make_mask(rows,4);
+F_imdata_sp = F_imdata.*mask;
 im_sp = ifft2(ifftshift(F_imdata_sp))/ft_weight;
 
 
@@ -24,7 +23,11 @@ im_sp = ifft2(ifftshift(F_imdata_sp))/ft_weight;
 [H1,V1,D1] = detcoef2('all',C,S,1); %take wavelet transform w/ haar filter, h,v,d are detail matrices
 A1 = appcoef2(C,S,'haar',1);
 
-thresh = 0.05*max(C);
+% thresh_pct = 0.8; %set bottom x% to zero, use 5% value as threshold
+% C_sorted = sort(C,'ascend');
+% thresh = abs(C_sorted(round(thresh_pct*size(C,2))));
+thresh = abs(0.025*max(C));
+
 for i = 1:size(C,2)
     if abs(C(i))<thresh
         C(i) = 0;
@@ -34,7 +37,20 @@ for i = 1:size(C,2)
 
 end
 
-im_sp_th = waverec2(C,S,'haar');
+im_sp_th = waverec2(C,S,'haar'); %reconstructed image after thresholding
+
+%go to k space and downsample
+F_sp_th = fftshift(fft2(im_sp_th)*ft_weight);
+F_sp_th_masked = F_sp_th.*mask;
+
+
+%find err in k space and ifft to get difference image
+F_err = F_imdata_sp - F_sp_th_masked; %y - Ax error between 2D FFT recon and wavelet recon in k space
+delta_im = ifft2(ifftshift(F_err))/ft_weight;
+
+%add diff im to original in image space
+new_im = im_sp - delta_im;
+new_F_imdata = fftshift(fft2(new_im)*ft_weight);
 
 %show images
 
@@ -71,5 +87,18 @@ title("Diagonal Detail Wavelet Coefficients")
 figure();
 imshow(abs(im_sp_th))
 title("reconstructed image after thresholding wavelet decompositions")
+
+figure();
+imshow(abs(F_sp_th))
+title("k space of thresholding");
+
+
+figure();
+imshow(abs(delta_im))
+title("Difference Image")
+
+figure();
+imshow(abs(new_im))
+title("Final Reconstructed Image after 1 loop")
 
 
